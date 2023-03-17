8054bc50d5254a4583ddfb7b815ba12cdec3c7df
guoyuze <guoyuze@100tal.com>
2023-03-17 11:36:02 +0800

    fix： 修改部署sh 脚本

--- deployed.sh
+++ deployed.sh
@@ -1,6 +1,30 @@
+# 容器名称
 CONTAINER_NAME="web-app"
+# 镜像名称
 IMAGE_NAME="client-app"
+
+# 检测是否支持指令
+function CommandSupportCheck(){
+    CommandSupport=`which $1`
+    echo $CommandSupport
+    if [ -z "$CommandSupport" ]; then
+        echo "当前环境不支持 $1 指令"
+        exit 0
+    fi
+}
+
+# 同步最新代码
+CommandSupportCheck "git"
+git pull
+
+# 初始化前端项目
+CommandSupportCheck "npm"
+echo "===>开始初始化项目"
+npm install
+echo "===>项目初始化完成"
+
 # 检查是否有 docker 环境
+CommandSupportCheck "docker"
 
 # 检测是否存在 正在运行的 的容器
 # docker inspect -f '{{.State.Status}}' $CONTAINER_NAME
